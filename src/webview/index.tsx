5c5c067f6ead2efa6a129d205c1fee84d1eb3498
Metrosoft <47860770+MetroSoft-Application@users.noreply.github.com>
2026-08-21 00:11:14 +0900

    Enable Markdown hover

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -7,6 +7,7 @@ import '@fontsource/noto-sans-jp/700.css';
 import '@fontsource/noto-sans-mono/400.css';
 import 'katex/dist/katex.min.css';
 import { App } from './App';
+import './cmMarkdownHover';
 import { getMessages } from '../shared/messages';
 
 /**
