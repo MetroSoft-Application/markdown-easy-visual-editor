1602c9f2487b2b44281ca20d3b2b6b8c85bf1d64
Metrosoft <47860770+MetroSoft-Application@users.noreply.github.com>
2026-08-29 01:05:59 +0900

    Install table editor overlay

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -11,9 +11,11 @@ import './cmMarkdownAutocomplete';
 import './editorTheme.css';
 import './editorThemePolish.css';
 import './previewFullWidth.css';
+import './tableEditorOverlay.css';
 import { getMessages } from '../shared/messages';
 import { installEditorThemeController } from './editorThemeController';
 import { installSelectedTextSearchTransfer } from './searchSelectedText';
+import { installTableEditorOverlay } from './tableEditorOverlay';
 
 /**
  * 行番号の左クリック完了後に、その論理行のテキスト全体を選択する。
@@ -40,6 +42,7 @@ if (!root) throw new Error(getMessages('en').internal.rootNotFound);
 root.addEventListener('click', handleLineNumberClick);
 installEditorThemeController();
 installSelectedTextSearchTransfer();
+installTableEditorOverlay();
 
 /** Reactのルート要素へアプリケーション本体をStrictMode付きで描画する。 */
 createRoot(root).render(
